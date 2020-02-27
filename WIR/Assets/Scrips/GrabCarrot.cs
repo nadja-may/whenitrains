c5fb8ce23adebbda68c64e034f51786876af192e
maypledew <nadjamaykramer@icloud.com>
2020-02-27 17:00:38 +0100

    Unity file import

---
+++ WIR/Assets/Scrips/GrabCarrot.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GrabCarrot : MonoBehaviour
+{
+    public GameObject carrot;
+
+    void OnMouseDown()
+    {
+        Destroy(carrot);
+    }
+}
