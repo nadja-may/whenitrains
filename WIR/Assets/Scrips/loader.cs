2466b2c6e6a22edf023e2cb873a544cec9bc9ed1
maypledew <nadjamaykramer@icloud.com>
2020-03-29 22:01:22 +0200

    first stable final version 4.0, added UI, pickup animation, cutscene finished, changed water, added 3D models, added more textures

---
+++ WIR/Assets/Scrips/loader.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void closeGame()
+    {
+        Application.Quit();
+        Debug.Log("closed");
+    }
+
+    public void toMenu()
+    {
+        SceneManager.LoadScene(0);
+        Debug.Log("backtomenu");
+    }
+
+    public void startExplain()
+    {
+        SceneManager.LoadScene(1);
+        Debug.Log("toexplain");
+    }
+    public void startGame()
+    {
+        SceneManager.LoadScene(2);
+        Debug.Log("togame");
+    }
+}
---
+++ WIR/Assets/Scrips/timelineTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class timelineTrigger : MonoBehaviour
+{
+    public PlayableDirector timeline;
+    public bool played = false;
+
+    void Start()
+    {
+        timeline = GetComponent<PlayableDirector>();
+    }
+
+    void OnTriggerEnter(Collider c)
+    {
+        if (c.gameObject.tag == "Player")
+        {
+            if (!played)
+            {
+                played = true;
+                timeline.Play();
+            }
+        }
+    }
+}
